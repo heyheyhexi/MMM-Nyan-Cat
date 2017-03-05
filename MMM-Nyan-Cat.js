62cbdcaa0f99e2c71390f40ec5ca65b3cf0be855
heyheyhexi <tobger@aol.com>
2017-03-05 18:24:15 +0100

    Create MMM-Nyan-Cat.js

---
+++ MMM-Nyan-Cat.js
@@ -0,0 +1,24 @@
+/* global Module */
+
+/* Magic Mirror
+ * Module: HelloWorld
+ *
+ * By Michael Teeuw http://michaelteeuw.nl
+ * MIT Licensed.
+ */
+
+Module.register("Nyan Cat",{
+
+	// Default module config.
+	defaults: {
+		"title": "Nyan Cat",
+     "gif": "https://img.clipartfest.com/9ee297869fc328d119a428b213ce95ef_data-visualization-virtual-clipart-gif-nyan-cat_1750-800.gif"
+	},
+
+	// Override dom generator.
+	getDom: function() {
+		var wrapper = document.createElement("div");
+		wrapper.innerHTML = this.config.text;
+		return wrapper;
+	}
+});
