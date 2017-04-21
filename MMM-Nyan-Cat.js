f6462fde6336989e6e8c05a28c5a1101599ed7cb
heyheyhexi <tobger@aol.com>
2017-04-21 23:00:23 +0200

    Update MMM-Nyan-Cat.js

--- MMM-Nyan-Cat.js
+++ MMM-Nyan-Cat.js
@@ -7,18 +7,17 @@
  * MIT Licensed.
  */
 
-Module.register("Nyan Cat",{
+Module.register("Nyan_Cat",{
 
 	// Default module config.
 	defaults: {
-		"title": "Nyan Cat",
-     "gif": "https://img.clipartfest.com/9ee297869fc328d119a428b213ce95ef_data-visualization-virtual-clipart-gif-nyan-cat_1750-800.gif"
+		"Header": "Nyan Cat",
+     		"gif": "https://img.clipartfest.com/9ee297869fc328d119a428b213ce95ef_data-visualization-virtual-clipart-gif-nyan-cat_1750-800.gif" ,
 	},
-
 	// Override dom generator.
 	getDom: function() {
 		var wrapper = document.createElement("div");
-		wrapper.innerHTML = this.config.text;
+		wrapper.innerHTML = this.config.gif;
 		return wrapper;
 	}
-});
+);
